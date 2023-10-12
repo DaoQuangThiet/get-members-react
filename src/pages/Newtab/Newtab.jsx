42e1428d13876f3bc50dc67f9d703638527672db
DaoQuangThiet <thietdq1711@gmail.com>
2023-10-12 17:59:38 +0700

    Reactjs

---
+++ src/pages/Newtab/Newtab.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+
+const Newtab = () => {
+  return (
+    <Layout>
+      <div className="my-8 p-5 bg-blue-500">
+        <h2 className="text-lg text-pink-300">Get member Group Facebook</h2>
+      </div>
+    </Layout>
+  );
+};
+
+export default Newtab;
---
+++ src/pages/Popup/Popup.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../../assets/img/logo.svg';
+import Greetings from '../../containers/Greetings/Greetings';
+
+const Popup = () => {
+  return (
+    <>
+      <header className="bg-blue-500 p-6">
+        <h2 className="text-center text-xl font-bold">
+          Get Members Groups Facebook
+        </h2>
+      </header>
+      <div className="p-6">
+        <h2 className="text-lg font-medium">Get Profile</h2>
+      </div>
+    </>
+  );
+};
+
+export default Popup;
